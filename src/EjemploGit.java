248113a64ba1a385373f9f907b17d78bd67844bc
JoseGalleg <119331098+JoseGalleg@users.noreply.github.com>
2022-12-12 15:45:55 +0100

    Suma

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -3,7 +3,9 @@ public class EjemploGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Hola Dam B");
+String nombre = "Jose Enrique";
+System.out.println(nombre);
 	}
 
 }
