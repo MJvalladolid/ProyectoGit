6aff244a3ea98042d5015795312c96943b956e41
MJ valladolid <mvalcar1711@g.educaand.es>
2022-12-12 15:53:04 +0100

    codigo de main

--- src/main/main.java
+++ src/main/main.java
@@ -1,10 +1,19 @@
 package main;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		int numero1, numero2, resultado;
+		System.out.print("Introduce primer número: ");
+		numero1 = sc.nextInt();
+		System.out.print("Introduce segundo número: ");
+		numero2 = sc.nextInt();
+		}
+		
 	}
 
-}
+
